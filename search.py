b4e86f93384da436567582a7bb73eda8c9c6a79d
Naila Shafirni Hidayat <naila.shafirni@ui.ac.id>
2025-05-10 00:14:58 +0700

    upload collection ke DB

--- search.py
+++ search.py
@@ -1,15 +1,49 @@
+import os
+from sqlalchemy import create_engine
+from urllib.parse import quote_plus
+import sys
+from dotenv import load_dotenv
+
 from index import SearchEngine   
+
+load_dotenv()
+
+
+# Database connection parameters - using your Supabase credentials
+db_params = {
+    'database': os.getenv('DB_NAME'),
+    'user': os.getenv('DB_USER'),  
+    'password': os.getenv('DB_PASSWORD'), 
+    'host': os.getenv('DB_HOST'),
+    'port': os.getenv('DB_PORT')
+}
+
+password_encoded = quote_plus(db_params['password'])
+connection_string = f"postgresql://{db_params['user']}:{password_encoded}@{db_params['host']}:{db_params['port']}/{db_params['database']}"
 
 if __name__ == "__main__":
     print("Job Search Engine")
     print("-" * 50)
+    try:
+        engine = create_engine(
+            connection_string,
+            connect_args={
+                'connect_timeout': 30,  # Longer timeout
+                'application_name': 'job_listings_import'  # Help identify this connection in logs
+            }
+        )
+        print("SQLAlchemy engine created successfully")
+    except Exception as e:
+        print(f"Error creating SQLAlchemy engine: {e}")
+        sys.exit(1)
+
     search_engine = SearchEngine()
     search_engine.load_index()
     search_engine.set_retrieval_model("BM25")
     
     # Perform searches
     print("\nSearch example 1: 'python programming'")
-    results = search_engine.search("python programming")
+    results = search_engine.search("python programming",engine=engine)
 
     try:
         display_cols = ['Job Id', 'Job Title', 'Job Description', 'score']
